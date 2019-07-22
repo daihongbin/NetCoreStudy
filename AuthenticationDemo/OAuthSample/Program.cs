dd9eea0691cc3c798abe2ae072cefcfade0b714c
daihb <daihb@DESKTOP-R31MQAB>
2019-07-22 16:01:32 +0800

    需要先学习IdentityServer4内容 https://www.cnblogs.com/RainingNight/p/oidc-authentication-in-asp-net-core.html

---
+++ AuthenticationDemo/OAuthSample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace OAuthSample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            new WebHostBuilder()
+                .UseKestrel()
+                .UseUrls("http://localhost:5001")
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .ConfigureLogging((hostingContext, logging) => { logging.AddConsole(); })
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
