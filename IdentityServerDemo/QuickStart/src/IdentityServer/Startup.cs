ed074682f5a5dc5041e8c819fd636b0931de57f4
daihb <daihb@DESKTOP-R31MQAB>
2019-07-25 13:18:00 +0800

    通过EFCore持久化IdentityServer的配置和操作数据。

--- IdentityServerDemo/QuickStart/src/IdentityServer/Startup.cs
+++ IdentityServerDemo/QuickStart/src/IdentityServer/Startup.cs
@@ -2,11 +2,15 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
-using System;
 using IdentityServer4;
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+using System.Reflection;
 
 namespace IdentityServer
 {
@@ -24,20 +28,51 @@ namespace IdentityServer
             // uncomment, if you want to add an MVC-based UI
             services.AddMvc().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_2_1);
 
-            var builder = services.AddIdentityServer()
-                .AddInMemoryIdentityResources(Config.GetIdentityResources())
-                .AddInMemoryApiResources(Config.GetApis())
-                .AddInMemoryClients(Config.GetClients())
-                .AddTestUsers(Config.GetUsers());
+            // 未使用EF Core之前的IdentityServer配置
+            //var builder = services.AddIdentityServer()
+            //    .AddInMemoryIdentityResources(Config.GetIdentityResources())
+            //    .AddInMemoryApiResources(Config.GetApis())
+            //    .AddInMemoryClients(Config.GetClients())
+            //    .AddTestUsers(Config.GetUsers());
 
-            if (Environment.IsDevelopment())
-            {
-                builder.AddDeveloperSigningCredential();
-            }
-            else
-            {
-                throw new Exception("need to configure key material");
-            }
+            //if (Environment.IsDevelopment())
+            //{
+            //    builder.AddDeveloperSigningCredential();
+            //}
+            //else
+            //{
+            //    throw new Exception("need to configure key material");
+            //}
+
+            //使用EFCore的IdentityServer
+            const string connectionString = @"Data Source=(LocalDb)\MSSQLLocalDB;database=IdentityServer4.Quickstart.EntityFramework-2.0.0;trusted_connection=yes;";
+            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
+            services.AddIdentityServer()
+                .AddDeveloperSigningCredential()
+                .AddTestUsers(Config.GetUsers())
+                .AddConfigurationStore(options => 
+                {
+                    options.ConfigureDbContext = builder =>
+                    {
+                        builder.UseSqlServer(connectionString, sql =>
+                        {
+                            sql.MigrationsAssembly(migrationsAssembly);
+                        });
+                    };
+                })
+                .AddOperationalStore(options => 
+                {
+                    options.ConfigureDbContext = builder =>
+                    {
+                        builder.UseSqlServer(connectionString, sql =>
+                         {
+                             sql.MigrationsAssembly(migrationsAssembly);
+                         });
+                    };
+                });
+
+
 
             services.AddAuthentication()
                 .AddMicrosoftAccount("MicrosoftAccount",options =>  //添加微软
@@ -78,6 +113,8 @@ namespace IdentityServer
                 app.UseDeveloperExceptionPage();
             }
 
+            InitializeDatabase(app);
+
             // uncomment if you want to support static files
             app.UseStaticFiles();
 
@@ -86,5 +123,45 @@ namespace IdentityServer
             // uncomment, if you want to add an MVC-based UI
             app.UseMvcWithDefaultRoute();
         }
+
+
+        #region private_method
+        private void InitializeDatabase(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+
+                var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+                context.Database.Migrate();
+                if (!context.Clients.Any())
+                {
+                    foreach (var client in Config.GetClients())
+                    {
+                        context.Clients.Add(client.ToEntity());
+                    }
+                }
+
+                if (!context.IdentityResources.Any())
+                {
+                    foreach (var resource in Config.GetIdentityResources())
+                    {
+                        context.IdentityResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.ApiResources.Any())
+                {
+                    foreach (var resource in Config.GetApis())
+                    {
+                        context.ApiResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+            }
+        }
+        #endregion private_method
     }
 }
