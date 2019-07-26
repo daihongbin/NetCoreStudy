27f3bcb9f9f9dabe240f916a1c749a72007c37e4
daihb <daihb@DESKTOP-R31MQAB>
2019-07-26 17:31:09 +0800

    solenovex's identityserver4.

---
+++ IdentityServerDemo/AnotherQuickStart/src/MVCClient/Startup.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MVCClient
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = "Cookies";
+                options.DefaultChallengeScheme = "oidc";
+            })
+            .AddCookie("Cookies")
+            .AddOpenIdConnect("oidc", options =>
+             {
+                 options.SignInScheme = "Cookies";
+                 options.Authority = "https://localhost:5001";
+                 options.RequireHttpsMetadata = true;
+                 options.ClientId = "mvcclient";
+                 options.ResponseType = "code id_token";
+                 options.Scope.Clear();
+                 options.Scope.Add("openid");
+
+                 options.SaveTokens = true;
+                 options.ClientSecret = "secret";
+             });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
---
+++ IdentityServerDemo/AnotherQuickStart/src/Michael.IdentityProvider/Config.cs
@@ -0,0 +1,67 @@
+﻿using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace Michael.IdentityProvider
+{
+    public static class Config
+    {
+        public static List<TestUser> GetUsers() => new List<TestUser>
+        {
+            new TestUser
+            {
+                SubjectId = "1",
+                Username = "Nick",
+                Password = "password",
+                Claims = new List<Claim>
+                {
+                    new Claim("given_name","Nick"),
+                    new Claim("family_name","Carter")
+                }
+            },
+            new TestUser
+            {
+                SubjectId = "2",
+                Username = "Dave",
+                Password = "password",
+                Claims = new List<Claim>
+                {
+                    new Claim("given_name","Dave"),
+                    new Claim("family_name","Mustaine")
+                }
+            }
+        };
+
+        public static IEnumerable<IdentityResource> GetIdentityResources() => new List<IdentityResource>
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile()
+        };
+
+        public static IEnumerable<Client> GetClients() => new List<Client>
+        {
+            new Client
+            {
+                ClientId = "mvcclient",
+                ClientName = "MVC 客户端",
+                AllowedGrantTypes = GrantTypes.Hybrid,
+
+                //登陆后跳转地址
+                RedirectUris = {"https://localhost:5002/signin-oidc"},
+
+                AllowedScopes = new List<string>
+                {
+                    IdentityServerConstants.StandardScopes.OpenId
+                },
+                ClientSecrets =
+                {
+                    new Secret("secret".Sha256())
+                }
+            }
+        };
+
+    }
+}
