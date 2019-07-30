5790c4d77d6924d10aee55eb5ee68245bb693496
daihb <daihb@DESKTOP-R31MQAB>
2019-07-30 15:02:34 +0800

    增加jwt相关案例

---
+++ JwtDemo/src/Michael.JwtProvider/Controllers/TokenController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Michael.JwtProvider.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        public ActionResult<string> Get()
+        {
+            var claims = new Claim[]
+            {
+                new Claim(ClaimTypes.Name,"Michael"),
+                new Claim(JwtRegisteredClaimNames.Email,"320459582@qq.com"),
+                new Claim(JwtRegisteredClaimNames.Sub,"D21D099B-B49B-4604-A247-71B0518A0B1C")
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("1234567890123456"));
+
+            var token = new JwtSecurityToken(
+                issuer: "http://localhost:5000",
+                audience: "http://localhost:5001",
+                claims: claims,
+                notBefore: DateTime.Now,
+                expires: DateTime.Now.AddHours(1),
+                signingCredentials: new SigningCredentials(key, SecurityAlgorithms.HmacSha256)
+                );
+
+            var jwtToken = new JwtSecurityTokenHandler().WriteToken(token);
+
+            //还可以这样创建Token
+            //var claims = new Claim[]
+            //{
+            //    new Claim(ClaimTypes.Name,"Michael"),
+            //    new Claim(JwtRegisteredClaimNames.Email,"320459582@qq.com"),
+            //    new Claim(JwtRegisteredClaimNames.Sub,"D21D099B-B49B-4604-A247-71B0518A0B1C"),
+            //    new Claim(JwtRegisteredClaimNames.Exp,$"{new DateTimeOffset(DateTime.Now.AddMilliseconds(1)).ToUnixTimeSeconds()}"),
+            //    new Claim(JwtRegisteredClaimNames.Nbf,$"{new DateTimeOffset(DateTime.Now).ToUnixTimeSeconds()}")
+            //};
+
+            //var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("1234567890123456"));
+
+            //var jwtToken = new JwtSecurityToken(new JwtHeader(new SigningCredentials(key, SecurityAlgorithms.HmacSha256)), new JwtPayload(claims));
+
+            return jwtToken;
+        }
+    }
+}
---
+++ JwtDemo/src/Michael.JwtWebApp/Startup.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Text;
+
+namespace Michael.JwtWebApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            //JwtBearerDefaults.AuthenticationScheme
+            var schemeName = "JwtBearerToken";
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = schemeName;
+                options.DefaultChallengeScheme = schemeName;
+            })
+           .AddJwtBearer(schemeName,options => 
+           {
+               options.TokenValidationParameters = new TokenValidationParameters
+               {
+                   ValidateIssuerSigningKey = true,
+                   IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("1234567890123456")),
+
+                   ValidateIssuer = true,
+                   ValidIssuer = "http://localhost:5000",
+
+                   ValidateAudience = true,
+                   ValidAudience = "http://localhost:5001",
+
+                   ValidateLifetime = true,
+
+                   ClockSkew = TimeSpan.FromMinutes(5)
+               };
+           });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseAuthentication();
+
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
