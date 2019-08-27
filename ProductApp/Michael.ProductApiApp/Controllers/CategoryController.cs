715155b49f9d66aa5cc0b82a6e8501bfed5644cc
daihb <daihb@DESKTOP-R31MQAB>
2019-08-27 11:21:23 +0800

    这就很蛋疼，你用url属性获取数据就能出现树表格效果。你自己拿到的数据放进去就没效果，真的哔了狗了。

--- ProductApp/Michael.ProductApiApp/Controllers/CategoryController.cs
+++ ProductApp/Michael.ProductApiApp/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
-﻿using Michael.ProductApiApp.Models;
+﻿using Michael.ProductApiApp.Entities;
+using Michael.ProductApiApp.Models;
 using Michael.ProductApiApp.Models.ViewModel;
 using Michael.ProductApiApp.Repositories;
 using Microsoft.AspNetCore.Mvc;
@@ -8,7 +9,9 @@ using System.Threading.Tasks;
 
 namespace Michael.ProductApiApp.Controllers
 {
-    [Route("api/[controller]")]
+    
+    [Produces("application/json")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class CategoryController : ControllerBase
     {
@@ -19,33 +22,77 @@ namespace Michael.ProductApiApp.Controllers
             _category = category;
         }
 
+        // 获取所有分类
         [HttpGet]
-        public async Task<ActionResult<List<CategoryViewModel>>> Get()
+        public async Task<Response<List<CategoryViewModel>>> GetAllCategories()
         {
-            var categories = await _category.GetAllCategory();
+            var response = new Response<List<CategoryViewModel>>();
 
-            var categoryViewModel = categories.Select(s => new CategoryViewModel
+            var categories = await _category.GetAllCategory();
+            if (categories == null || categories.Count <= 0)
+            {
+                response.Code = "500";
+                response.Msg = "未获取到分类";
+                return response;
+            }
+
+            var categoryVMs = categories.Select(s => new CategoryViewModel
             {
                 CategoryId = s.CategoryID,
                 ParentId = s.ParentID,
                 CategoryName = s.CategoryName,
                 ViewOrder = s.ViewOrder,
                 Description = s.Description
-            });
+            }).ToList();
 
-            return new JsonResult(categoryViewModel);
+            response.Code = "200";
+            response.Msg = "获取分类成功！";
+            response.Data = categoryVMs;
+            return response;
         }
 
-        [Route("{id}")]
-        public async Task<ActionResult<CategoryViewModel>> Get(int id)
+        // 获取所有分类
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
         {
+            var response = new Response<List<CategoryViewModel>>();
+
+            var categories = await _category.GetAllCategory();
+            if (categories == null || categories.Count <= 0)
+            {
+                response.Code = "500";
+                response.Msg = "未获取到分类";
+                return NotFound(response.Data);
+            }
+
+            var categoryVMs = categories.Select(s => new CategoryViewModel
+            {
+                CategoryId = s.CategoryID,
+                ParentId = s.ParentID,
+                CategoryName = s.CategoryName,
+                ViewOrder = s.ViewOrder,
+                Description = s.Description
+            }).ToList();
+
+            response.Code = "200";
+            response.Msg = "获取分类成功！";
+            response.Data = categoryVMs;
+            return Ok(response.Data);
+        }
+
+        public async Task<Response<CategoryViewModel>> GetCategoryById(int id)
+        {
+            var response = new Response<CategoryViewModel>();
+
             var category = await _category.GetCategoryById(id);
             if (category == null)
             {
-                return NotFound();
+                response.Code = "500";
+                response.Msg = $"获取编号为{id}的分类失败！";
+                return response;
             }
 
-            var categoryVM = new CategoryViewModel
+            var categoryVm = new CategoryViewModel
             {
                 CategoryId = category.CategoryID,
                 ParentId = category.ParentID,
@@ -53,15 +100,21 @@ namespace Michael.ProductApiApp.Controllers
                 Description = category.Description,
                 ViewOrder = category.ViewOrder
             };
-            return new JsonResult(categoryVM);
+            response.Code = "200";
+            response.Msg = $"获取编号为{id}的分类成功！";
+            response.Data = categoryVm;
+            return response;
         }
 
         [HttpPost]
-        public async Task<ActionResult> Post(CategoryViewModel categoryViewModel)
+        public async Task<Response<string>> AddCategory(CategoryViewModel categoryViewModel)
         {
+            var response = new Response<string>();
             if (categoryViewModel == null)
             {
-                return BadRequest();
+                response.Code = "401";
+                response.Msg = "添加分类失败";
+                return response;
             }
 
             var category = new Category
@@ -73,7 +126,10 @@ namespace Michael.ProductApiApp.Controllers
                 Description = categoryViewModel.Description
             };
             await _category.AddCategory(category);
-            return Ok();
+
+            response.Code = "200";
+            response.Msg = "添加分类成功";
+            return response;
         }
     }
 }
---
+++ ProductApp/Michael.ProductApiApp/Entities/Response.cs
@@ -0,0 +1,11 @@
+﻿namespace Michael.ProductApiApp.Entities
+{
+    public class Response<T>
+    {
+        public string Code { get; set; }
+
+        public string Msg { get; set; }
+
+        public T Data { get; set; }
+    }
+}
